cb5f79bf9b9a08e79706d7a10214d73898bee102
bilaimut :D <izahpplg@gmail.com>
2026-03-05 10:20:12 +0700

    Update proseslogin.php

--- proseslogin.php
+++ proseslogin.php
@@ -5,9 +5,6 @@ include 'koneksi.php';
  $user_input = mysqli_real_escape_string($koneksi, $_POST['username']);
  $password   = mysqli_real_escape_string($koneksi, $_POST['password']);
 
-// --- BENERIN QUERY DISINI ---
-// Logika: Cari user yang (Username = input) ATAU (NIS = input)
-// Jadi dia bisa login pakai username 'admin' ATAU pakai NIS '12345'
  $sql = "SELECT * FROM tbuser WHERE (username='$user_input' OR nis='$user_input') AND password='$password'";
  $query = mysqli_query($koneksi, $sql);
  $data = mysqli_fetch_assoc($query);
@@ -21,14 +18,10 @@ if($data) {
     
     // Cek Role buat arahin halaman
     if($data['role'] == 'admin'){
-        // Kalau Admin, arahkan ke folder admin (sesuaikan kalau file ada di root, hapus 'admin/')
-        // Karena tadi kita buat dashboardadmin.php di root, jadi langsung 'dashboardadmin.php'
         header("Location: admin/dashboardadmin.php"); 
         exit; 
     }
     elseif($data['role'] == 'siswa'){
-        // Kalau Siswa, arahkan ke dashboard siswa (index.php atau dashboardsiswa.php)
-        // Sesuai yang kita buat tadi, dashboard siswa ada di index.php
         header("Location: dashboardsiswa.php"); 
         exit; 
     } else {
@@ -40,4 +33,5 @@ if($data) {
     // Kalau Username/NIS atau Password salah
     echo "<script>alert('Username/NIS atau Password Salah!'); window.location='indexlogin.php';</script>";
 }
+
 ?>
